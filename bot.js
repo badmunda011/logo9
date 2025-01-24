35416e40645c6f7b0c081fad6e28b66ca17024ca
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-01-24 16:56:36 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ instagram : adimas_shadoet
 */
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('Your_Token')
+const bot = new Telegraf('7961009610:AAEyVoy23DM9F3u3goLmJCqYJhXFNa4XY2Y')
 const axios = require('axios')
 const helpMessage = `Katakan sesuatu kepada saya\n/start - untuk memulai bot\n/menu - untuk menampilkan list menu`;
 bot.use((ctx, next) => {
